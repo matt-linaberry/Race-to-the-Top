83e60f7d9ef584fda84b135dfc0eaa72f49806f5
Matt Linaberry <matt.linaberry@we-ics.com>
2014-12-13 15:47:22 -0500

    The game is done!

--- Race to the Top/ViewController.m
+++ Race to the Top/ViewController.m
@@ -9,9 +9,15 @@
 #import "ViewController.h"
 #import "RTPathView.h"
 #import "RTMountainPath.h"
+
+#define RTMAP_STARTING_SCORE 15000
+#define RTMAP_SCORE_DECREMENT_AMOUNT 100
+#define RTTIMER_INTERVAL 0.1
+#define RTWALL_PENALTY 500
+
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet RTPathView *pathView;
-
+@property (strong, nonatomic) NSTimer *timer;
 @end
 
 @implementation ViewController
@@ -25,6 +31,7 @@
     
     UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panDetected:)];
     [self.pathView addGestureRecognizer:panRecognizer];
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", RTMAP_STARTING_SCORE];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -44,15 +51,51 @@
 - (void) panDetected:(UIPanGestureRecognizer *) panRecognizer
 {
     CGPoint fingerLocation = [panRecognizer locationInView:self.pathView];
-    for (UIBezierPath *path in [RTMountainPath mountainPathsForRect:self.pathView.bounds])
+    if (panRecognizer.state == UIGestureRecognizerStateBegan && fingerLocation.y < 750)
     {
-        UIBezierPath *tapTarget = [RTMountainPath tapTargetForPath:path];
-        if ([tapTarget containsPoint:fingerLocation])
-        {
-            NSLog(@"You banged your head into the wall!!!");
-        }
-        
+        //start this up.
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:RTTIMER_INTERVAL target:self selector:@selector(timerFired) userInfo:nil repeats:YES];
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", RTMAP_STARTING_SCORE];
     }
+    else if (panRecognizer.state == UIGestureRecognizerStateChanged)
+    {
+        for (UIBezierPath *path in [RTMountainPath mountainPathsForRect:self.pathView.bounds])
+        {
+            UIBezierPath *tapTarget = [RTMountainPath tapTargetForPath:path];
+            if ([tapTarget containsPoint:fingerLocation])
+            {
+                [self decrementScoreByAmount:RTWALL_PENALTY];
+            }
+            
+        }
+    }
+    else if (panRecognizer.state == UIGestureRecognizerStateEnded && fingerLocation.y <= 165)
+    {
+        [self.timer invalidate]; // cancel the timer
+        self.timer = nil;  //release it
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Please start at the bottom of the path and keep your finger held down, cheater!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+        [self.timer invalidate];
+        self.timer = nil;
+    }
+
 }
 
+-(void) timerFired
+{
+    [self decrementScoreByAmount:RTMAP_SCORE_DECREMENT_AMOUNT];
+}
+
+-(void) decrementScoreByAmount:(int)amount
+{
+    NSString *scoreText = [[self.scoreLabel.text componentsSeparatedByString:@" "] lastObject];  // this will return the LAST token in this string!
+    //convert to int
+    int score = [scoreText intValue];
+    score = score - amount;
+    NSString *newScoreText = [self.scoreLabel.text stringByReplacingOccurrencesOfString:scoreText withString:[NSString stringWithFormat:@"%i", score]];
+    self.scoreLabel.text = newScoreText;
+}
 @end
